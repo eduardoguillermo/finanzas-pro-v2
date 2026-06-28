9340360c3a06c5bb7089ea6cb7ccbe9e9e60a21f
eduardoguillermo <70851744+eduardoguillermo@users.noreply.github.com>
2026-06-27 22:41:58 -0300

    sw: v2.02

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finanzas-pro-v2-2.01';
+const CACHE = 'finanzas-pro-v2-2.02';
 const ASSETS = ['./index.html', './manifest.json', './xlsx.full.min.js'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => Promise.all(ASSETS.map(a => fetch(a+'?v='+Date.now(),{cache:'no-store'}).then(r=>{if(r.ok)c.put(a,r);}).catch(()=>{}))))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
